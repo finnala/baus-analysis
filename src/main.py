780e06c551f6ceb2e41e74e4fb77b9d6086adfca
finnala <122728490+finnala@users.noreply.github.com>
2023-06-29 16:15:17 +0100

    Retrieve match ids in main.py

--- src/main.py
+++ src/main.py
@@ -8,3 +8,11 @@ load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 client = HTTPClient(API_KEY)
 
+player_puuid = client.get_puuid("Thebausffs")
+
+match_ids = list()
+intervals = [0, 100, 200, 300, 400]
+
+for interval in intervals:
+    match_list = client.get_match_ids(player_puuid, 100, interval)
+    match_ids += match_list
