547ea3c91bb8739ed7f97ca0a57a2c50e0b35ba2
finnala <122728490+finnala@users.noreply.github.com>
2023-06-29 19:25:45 +0100

    Create json data for matches

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 from client import HTTPClient
+from save_json_data import create_data_dir, match_data_to_json
 
 
 load_dotenv()
@@ -16,3 +18,14 @@ intervals = [0, 100, 200, 300, 400]
 for interval in intervals:
     match_list = client.get_match_ids(player_puuid, 100, interval)
     match_ids += match_list
+
+create_data_dir()
+
+for match in match_ids:
+    data_dir = Path.cwd() / "data"
+    json_paths = [path for path in data_dir.iterdir() if path.suffix == '.json']
+    json_match_ids = [path.stem for path in json_paths]
+
+    if match not in json_match_ids:
+        print(f"Creating file for match {match}")
+        match_data_to_json(client, match)
