0916d9d5bc2365dd763baef9babe3de1c7f325dc
finnala <122728490+finnala@users.noreply.github.com>
2023-06-30 11:17:51 +0100

    Create functions to find and curate player stats

--- src/extract_json.py
+++ src/extract_json.py
@@ -13,3 +13,21 @@ def get_matches(json_paths: list) -> list:
         match_data = json_to_dict(path)
         matches.append(match_data)
     return matches
+
+
+def is_player(player: dict, summoner_name: str) -> bool:
+    return player["summonerName"] == summoner_name
+
+
+# Returns the entire player stats
+def get_player_stats(players: list, summoner_name: str) -> list:
+    return list(filter(lambda x: is_player(x, summoner_name), players))
+
+
+def curate_stats(player: dict, desired_stats: list, desired_challenges: list):
+    all_stats = []
+    for stat in desired_stats:
+        all_stats.append(player[stat])
+    for chal in desired_challenges:
+        all_stats.append(player["challenges"][chal])
+    return all_stats
